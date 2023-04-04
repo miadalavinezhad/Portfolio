7462df0c8a093cc3568492b8d41ed54b64e7c7a8
miadalavinezhad <miadalavinezhad@gmail.com>
2023-04-04 20:14:10 +0200

    contact page complete v1

--- script.js
+++ script.js
@@ -3,28 +3,34 @@ const about_me = document.getElementById("aAboutMe");
 const skills = document.getElementById("aSkills");
 const contact = document.getElementById("aContact");
 
+// About me
 about_me.onmouseenter = event =>
 {
     event.target.style.borderBottom = "2px solid #484b6a";
 };
+
 about_me.onmouseleave = event =>
 {
     event.target.style.borderBottom = "";
 };
 
+// Skills
 skills.onmouseenter = event =>
 {
     event.target.style.borderBottom = "2px solid #484b6a";
 };
+
 skills.onmouseleave = event =>
 {
     event.target.style.borderBottom = "";
 };
 
+// Contact
 contact.onmouseenter = event =>
 {
     event.target.style.borderBottom = "2px solid #484b6a";
 };
+
 contact.onmouseleave = event =>
 {
     event.target.style.borderBottom = "";
@@ -37,6 +43,7 @@ html_logo.onmouseenter = event =>
 {
     event.target.src = "img/html-5.png";
 };
+
 html_logo.onmouseleave = event => 
 {
     event.target.src = "img/htmlBW.png";
@@ -48,6 +55,7 @@ css_logo.onmouseenter = event =>
 {
     event.target.src = "img/css-3.png";
 };
+
 css_logo.onmouseleave = event => 
 {
     event.target.src = "img/css-3BW.png";
@@ -59,6 +67,7 @@ git_logo.onmouseenter = event =>
 {
     event.target.src = "img/git.png";
 };
+
 git_logo.onmouseleave = event => 
 {
     event.target.src = "img/gitBW.png";
@@ -70,7 +79,20 @@ python_logo.onmouseenter = event =>
 {
     event.target.src = "img/python.png";
 };
+
 python_logo.onmouseleave = event => 
 {
     event.target.src = "img/pythonBW.png";
+};
+
+// Change js logo color
+const js_logo = document.getElementById("js");
+js_logo.onmouseenter = event => 
+{
+    event.target.src = "img/js.png";
+};
+
+js_logo.onmouseleave = event => 
+{
+    event.target.src = "img/jsBW.png";
 };
